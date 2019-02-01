bd630c7ecf5b30054e5300ffc91e8588316298be
Despres Mathis <s171162@ic2s120-10.univ-lemans.fr>
2019-02-01 11:59:17 +0100

    Commit from Univ

--- test_liste.c
+++ test_liste.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <individu.h>
 #include <fraction.h>
 #include <mystring.h>
 #include <liste.h>
 
-#define N 5 
+#include <getopt.h>
+
+
+/* ------------------------------ */
+/* ----- long options array ----- */
+/* ------------------------------ */
+
+static struct option longopts[] =
+{
+ /* {"verbose", required_argument, NULL, (int)'v'},*/
+  {"verbose", no_argument, NULL, (int)'v'},
+  {"help", no_argument, NULL, (int)'h'},
+  {"option-a", no_argument, NULL, (int)'a'},
+  {"option-b", no_argument, NULL, (int)'b'},
+  {"option-c", required_argument, NULL, (int)'c'},
+  {0, 0, 0, 0}
+};
+
+
+/*
+ * Usage 
+ */
+static 
+void usage( char * nomprog )
+{
+      fprintf( stdout , "Programme de test des options requerant 3 arguments (quelconques) \n");
+      fprintf( stdout , "usage : %s [options]  argument1 argument2 argument3\n" , nomprog );
+      fprintf( stdout , "Options:\n"); 
+      fprintf( stdout , "  -v, --verbose <niveau>        niveau de la trace de l'execution\n");
+      fprintf( stdout , "                 0              pas de trace\n");
+      fprintf( stdout , "                 1              trace des actions principales\n");
+      fprintf( stdout , "                 2              trace detaillee\n");
+      fprintf( stdout , "  -h, --help                    affiche cette aide\n");
+      fprintf( stdout , "  -a, --option-a                positionne le flag 'a'\n");
+      fprintf( stdout , "  -b, --option-b                positionne le flag 'b'\n");
+      fprintf( stdout , "  -c, --option-c <valeur>       affecte l'option 'c'\n");
+}
+
+
+#define N atoi(argv[1]) 
 
 int
-main( ) 
+main(int argc, char * argv[]) 
 {
   err_t noerr = OK; 
 
@@ -23,8 +62,71 @@ main( )
   individus = malloc( sizeof(individu_t *) * N )  ; 
   fractions = malloc( sizeof(fraction_t *) * N )  ;
   strings = malloc( sizeof(string_t *) * N )   ; 
+  
+  /*Debut de test avec les options*/
+  int opt;
+  char nomprog[512] ; 
+  int verbose = 0 ;
 
-  printf( "Debut du programme des test sur les listes de %d objets homogenes\n" , N ) ; 
+  int aflag = 0;
+  int bflag = 0;
+  char *cvalue = NULL;
+    /* Section options */
+  strcpy( nomprog , argv[0] ) ;
+  while ((opt = getopt_long(argc, argv, "v:habc:", longopts, NULL)) != -1 ){
+    switch(opt){
+      
+      case 'v': /* --verbose */
+        verbose = atoi(optarg) ;
+        /*switch(verbose) 
+          {
+          case 0 : 
+            break ;
+          case 1 : printf("Trace niveau 1 : principales actions\n");
+            break ;
+          case 2 : printf("Trace niveau 2 : ttrace detaillee\n");
+            break ;
+          default : printf("%s : niveau de trace invalide [%d]\n", nomprog , verbose );
+            usage(nomprog) ;
+            exit(0);
+          }*/
+        break;
+
+      case 'h': /* --help */
+          usage(nomprog) ;
+          exit(0);
+          break;
+
+        case 'a' :
+          aflag = 1;
+          break;
+
+        case 'b' :
+          bflag = 1;
+          break;
+
+        case 'c':
+          cvalue = malloc( strlen(optarg)+1 ) ; 
+          strcpy( cvalue , optarg ) ; 
+          break;
+
+        case '?':
+          usage(nomprog) ;
+          exit(0);
+          break;
+
+        default:
+          usage(nomprog) ;
+          exit(0);
+          break;
+        }
+  }
+  argc -= optind ;
+  argv += optind ;
+
+  /* Fin des tests avec options */
+  if(verbose)
+    printf( "Debut du programme des test sur les listes de %d objets homogenes\n" , N ) ; 
 
 
  /* ---------- */
